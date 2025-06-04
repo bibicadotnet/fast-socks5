3e883d23e48c95c8ec6191c96597d67dc98c8a37
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-06-05 03:43:32 +0700

    Update server_auth_once.rs

--- examples/server_auth_once.rs
+++ examples/server_auth_once.rs
@@ -1,10 +1,6 @@
-#[forbid(unsafe_code)]
-#[macro_use]
-extern crate log;
-
 use anyhow::Context;
 use fast_socks5::{
-    server::{run_tcp_proxy, run_udp_proxy, DnsResolveHelper as _, Socks5ServerProtocol},
+    server::{run_tcp_proxy, run_udp_proxy, Socks5ServerProtocol},
     ReplyError, Result, Socks5Command, SocksError,
 };
 use std::collections::HashSet;
@@ -16,71 +12,36 @@ use tokio::net::TcpListener;
 use tokio::sync::RwLock;
 use tokio::task;
 
-/// # How to use it:
-///
-/// Listen on a local address, authentication-free:
-///     `$ RUST_LOG=debug cargo run --example server -- --listen-addr 127.0.0.1:1337 no-auth`
-///
-/// Listen on a local address, with basic username/password requirement:
-///     `$ RUST_LOG=debug cargo run --example server -- --listen-addr 127.0.0.1:1337 password --username admin --password password`
-///
-/// Same as above but with UDP support
-///     `$ RUST_LOG=debug cargo run --example server -- --listen-addr 127.0.0.1:1337 --allow-udp --public-addr 127.0.0.1 password --username admin --password password`
-///
-/// Listen with one-time authentication (IP whitelist after first auth):
-///     `$ RUST_LOG=debug cargo run --example server -- --listen-addr 127.0.0.1:1337 --auth-once password --username admin --password password`
 #[derive(Debug, StructOpt)]
-#[structopt(
-    name = "socks5-server",
-    about = "A simple implementation of a socks5-server."
-)]
 struct Opt {
-    /// Bind on address address. eg. `127.0.0.1:1080`
     #[structopt(short, long)]
     pub listen_addr: String,
-
-    /// Our external IP address to be sent in reply packets (required for UDP)
     #[structopt(long)]
     pub public_addr: Option<std::net::IpAddr>,
-
-    /// Request timeout
     #[structopt(short = "t", long, default_value = "10")]
     pub request_timeout: u64,
-
-    /// Choose authentication type
-    #[structopt(subcommand, name = "auth")] // Note that we mark a field as a subcommand
+    #[structopt(subcommand)]
     pub auth: AuthMode,
-
-    /// Don't perform the auth handshake, send directly the command request
     #[structopt(short = "k", long)]
     pub skip_auth: bool,
-
-    /// Allow UDP proxying, requires public-addr to be set
     #[structopt(short = "U", long)]
     pub allow_udp: bool,
-
-    /// Enable one-time authentication - IP whitelist after successful auth
     #[structopt(long)]
     pub auth_once: bool,
 }
 
-/// Choose the authentication type
 #[derive(StructOpt, Debug, PartialEq)]
 enum AuthMode {
     NoAuth,
     Password {
         #[structopt(short, long)]
         username: String,
-
         #[structopt(short, long)]
         password: String,
     },
 }
 
-/// Shared state for authenticated IPs
-#[derive(Debug, Clone)]
 struct AuthState {
-    /// Set of IP addresses that have been successfully authenticated
     authenticated_ips: Arc<RwLock<HashSet<IpAddr>>>,
 }
 
@@ -91,220 +52,96 @@ impl AuthState {
         }
     }
 
-    /// Check if an IP is already authenticated
     async fn is_authenticated(&self, ip: &IpAddr) -> bool {
-        let ips = self.authenticated_ips.read().await;
-        ips.contains(ip)
+        self.authenticated_ips.read().await.contains(ip)
     }
 
-    /// Add an IP to the authenticated list
     async fn add_authenticated_ip(&self, ip: IpAddr) {
-        let mut ips = self.authenticated_ips.write().await;
-        if ips.insert(ip) {
-            info!("IP {} added to whitelist after successful authentication", ip);
-        }
-    }
-
-    /// Get count of authenticated IPs (for logging)
-    async fn authenticated_count(&self) -> usize {
-        let ips = self.authenticated_ips.read().await;
-        ips.len()
-    }
-
-    /// Remove an IP from the authenticated list (for testing or management)
-    #[allow(dead_code)]
-    async fn remove_authenticated_ip(&self, ip: &IpAddr) -> bool {
-        let mut ips = self.authenticated_ips.write().await;
-        let removed = ips.remove(ip);
-        if removed {
-            info!("IP {} removed from whitelist", ip);
-        }
-        removed
-    }
-
-    /// Clear all authenticated IPs (for testing or management)
-    #[allow(dead_code)]
-    async fn clear_all(&self) {
-        let mut ips = self.authenticated_ips.write().await;
-        let count = ips.len();
-        ips.clear();
-        info!("Cleared {} IPs from whitelist", count);
+        self.authenticated_ips.write().await.insert(ip);
     }
 }
 
-/// Useful read 1. https://blog.yoshuawuyts.com/rust-streams/
-/// Useful read 2. https://blog.yoshuawuyts.com/futures-concurrency/
-/// Useful read 3. https://blog.yoshuawuyts.com/streams-concurrency/
-/// error-libs benchmark: https://blog.yoshuawuyts.com/error-handling-survey/
-///
-/// TODO: Write functional tests: https://github.com/ark0f/async-socks5/blob/master/src/lib.rs#L762
-/// TODO: Write functional tests with cURL?
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
-
-    spawn_socks_server().await
-}
-
-async fn spawn_socks_server() -> Result<()> {
     let opt: &'static Opt = Box::leak(Box::new(Opt::from_args()));
     
-    // Validation checks
     if opt.allow_udp && opt.public_addr.is_none() {
-        return Err(SocksError::ArgumentInputError(
-            "Can't allow UDP if public-addr is not set",
-        ));
+        return Err(SocksError::ArgumentInputError(""));
     }
     if opt.skip_auth && opt.auth != AuthMode::NoAuth {
-        return Err(SocksError::ArgumentInputError(
-            "Can't use skip-auth flag and authentication altogether.",
-        ));
+        return Err(SocksError::ArgumentInputError(""));
     }
     if opt.auth_once && opt.auth == AuthMode::NoAuth {
-        return Err(SocksError::ArgumentInputError(
-            "Can't use auth-once with no-auth mode. Use password authentication with auth-once.",
-        ));
+        return Err(SocksError::ArgumentInputError(""));
     }
     if opt.auth_once && opt.skip_auth {
-        return Err(SocksError::ArgumentInputError(
-            "Can't use auth-once with skip-auth flag.",
-        ));
+        return Err(SocksError::ArgumentInputError(""));
     }
 
     let listener = TcpListener::bind(&opt.listen_addr).await?;
     let auth_state = AuthState::new();
 
-    info!("Listen for socks connections @ {}", &opt.listen_addr);
-    if opt.auth_once {
-        info!("One-time authentication enabled - IPs will be whitelisted after successful auth");
-    }
-
-    // Standard TCP loop
     loop {
         match listener.accept().await {
             Ok((socket, client_addr)) => {
                 let auth_state_clone = auth_state.clone();
-                spawn_and_log_error(serve_socks5(opt, socket, client_addr.ip(), auth_state_clone));
+                task::spawn(async move {
+                    if let Err(e) = handle_connection(opt, socket, client_addr.ip(), auth_state_clone).await {
+                        let _ = e;
+                    }
+                });
             }
-            Err(err) => {
-                error!("accept error = {:?}", err);
-            }
+            Err(_) => continue,
         }
     }
 }
 
-async fn serve_socks5(
-    opt: &Opt, 
-    socket: tokio::net::TcpStream, 
+async fn handle_connection(
+    opt: &Opt,
+    socket: tokio::net::TcpStream,
     client_ip: IpAddr,
-    auth_state: AuthState
-) -> Result<(), SocksError> {
-    
-    // CRITICAL FIX: Determine the effective authentication method BEFORE negotiation
-    let effective_auth_method = determine_effective_auth_method(opt, &client_ip, &auth_state).await;
-    
-    debug!("Client {} connecting. Effective auth method: {:?}", 
-           client_ip, effective_auth_method);
-
-    // Perform authentication based on the effective method
-    let (proto, cmd, target_addr) = match effective_auth_method {
-        EffectiveAuthMethod::NoAuth => {
+    auth_state: AuthState,
+) -> Result<()> {
+    let proto = match &opt.auth {
+        AuthMode::NoAuth => {
             if opt.skip_auth {
-                debug!("Using skip-auth mode for {}", client_ip);
                 Socks5ServerProtocol::skip_auth_this_is_not_rfc_compliant(socket)
             } else {
-                debug!("Using no-auth method for {}", client_ip);
                 Socks5ServerProtocol::accept_no_auth(socket).await?
             }
         }
-        EffectiveAuthMethod::PasswordRequired { username, password } => {
-            debug!("Requiring password authentication for {}", client_ip);
-            let (proto, auth_success) = Socks5ServerProtocol::accept_password_auth(socket, |user, pass| {
-                let is_valid = user == username && pass == password;
-                debug!("Password auth attempt for {} - user: '{}', valid: {}", 
-                       client_ip, user, is_valid);
-                is_valid
-            }).await?;
+        AuthMode::Password { username, password } => {
+            if opt.auth_once && auth_state.is_authenticated(&client_ip).await {
+                Socks5ServerProtocol::accept_no_auth(socket).await?
+            } else {
+                let (proto, auth_success) = Socks5ServerProtocol::accept_password_auth(socket, |u, p| {
+                    u == username && p == password
+                }).await?;
 
-            // If auth was successful and auth_once is enabled, add IP to whitelist
-            if auth_success && opt.auth_once {
-                auth_state.add_authenticated_ip(client_ip).await;
-                let count = auth_state.authenticated_count().await;
-                info!("Authentication successful for {}. Total whitelisted IPs: {}", 
-                      client_ip, count);
+                if auth_success && opt.auth_once {
+                    auth_state.add_authenticated_ip(client_ip).await;
+                }
+                
+                proto
             }
-            
-            proto
         }
-    }
-    .read_command()
-    .await?
-    .resolve_dns()
-    .await?;
+    };
 
-    // Handle the command
+    let (proto, cmd, target_addr) = proto.read_command().await?.resolve_dns().await?;
+
     match cmd {
         Socks5Command::TCPConnect => {
-            debug!("Handling TCP connect for {} to {}", client_ip, target_addr);
             run_tcp_proxy(proto, &target_addr, opt.request_timeout, false).await?;
         }
         Socks5Command::UDPAssociate if opt.allow_udp => {
-            debug!("Handling UDP associate for {} to {}", client_ip, target_addr);
-            let reply_ip = opt.public_addr.context("invalid reply ip")?;
+            let reply_ip = opt.public_addr.context("")?;
             run_udp_proxy(proto, &target_addr, None, reply_ip, None).await?;
         }
         _ => {
-            warn!("Unsupported command from {}: {:?}", client_ip, cmd);
             proto.reply_error(&ReplyError::CommandNotSupported).await?;
             return Err(ReplyError::CommandNotSupported.into());
         }
     };
     
-    debug!("Connection completed successfully for {}", client_ip);
     Ok(())
-}
-
-/// Represents the effective authentication method to use for a client
-#[derive(Debug)]
-enum EffectiveAuthMethod {
-    NoAuth,
-    PasswordRequired { username: String, password: String },
-}
-
-/// Determine which authentication method should be used for this client
-async fn determine_effective_auth_method(
-    opt: &Opt,
-    client_ip: &IpAddr,
-    auth_state: &AuthState,
-) -> EffectiveAuthMethod {
-    match &opt.auth {
-        AuthMode::NoAuth => {
-            debug!("Configuration: NoAuth mode");
-            EffectiveAuthMethod::NoAuth
-        }
-        AuthMode::Password { username, password } => {
-            if opt.auth_once && auth_state.is_authenticated(client_ip).await {
-                info!("IP {} is whitelisted - bypassing authentication", client_ip);
-                EffectiveAuthMethod::NoAuth
-            } else {
-                debug!("IP {} requires password authentication", client_ip);
-                EffectiveAuthMethod::PasswordRequired {
-                    username: username.clone(),
-                    password: password.clone(),
-                }
-            }
-        }
-    }
-}
-
-fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
-where
-    F: Future<Output = Result<()>> + Send + 'static,
-{
-    task::spawn(async move {
-        match fut.await {
-            Ok(()) => {}
-            Err(err) => error!("Connection error: {:#}", &err),
-        }
-    })
 }
