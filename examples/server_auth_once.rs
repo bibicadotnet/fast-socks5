9d1bb3fd11489fe809145a969d969dd1ce35d979
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-06-05 07:05:07 +0700

    Update server_auth_once.rs

--- examples/server_auth_once.rs
+++ examples/server_auth_once.rs
@@ -1,6 +1,4 @@
 #![forbid(unsafe_code)]
-#[macro_use]
-extern crate log;
 
 use anyhow::Context;
 use fast_socks5::{
@@ -35,7 +33,7 @@ struct Opt {
     pub request_timeout: u64,
 
     /// Choose authentication type
-    #[structopt(subcommand, name = "auth")] 
+    #[structopt(subcommand, name = "auth")]
     pub auth: AuthMode,
 
     /// Don't perform the auth handshake, send directly the command request
@@ -82,15 +80,12 @@ impl AuthState {
 
     async fn add_ip(&self, ip: IpAddr) {
         let mut ips = self.whitelisted_ips.write().await;
-        if ips.insert(ip) {
-            info!("Added IP {} to whitelist", ip);
-        }
+        ips.insert(ip);
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
     spawn_socks_server().await
 }
 
@@ -116,19 +111,14 @@ async fn spawn_socks_server() -> Result<()> {
     let auth_state = AuthState::new();
     let listener = TcpListener::bind(&opt.listen_addr).await?;
 
-    info!("Listen for socks connections @ {}", &opt.listen_addr);
-    if opt.auth_once {
-        info!("One-time authentication enabled - IPs will be whitelisted after first successful auth");
-    }
-
     loop {
         match listener.accept().await {
             Ok((socket, client_addr)) => {
                 let client_ip = client_addr.ip();
                 spawn_and_log_error(serve_socks5(opt, socket, client_ip, auth_state.clone()));
             }
-            Err(err) => {
-                error!("accept error = {:?}", err);
+            Err(_) => {
+                // silently ignore accept errors
             }
         }
     }
@@ -140,25 +130,21 @@ async fn serve_socks5(
     client_ip: IpAddr,
     auth_state: AuthState,
 ) -> Result<(), SocksError> {
-    // If auth_once mode is enabled and IP is whitelisted, skip any auth negotiation,
-    // just accept whatever client sends as auth (NO_AUTH, or user/pass).
-    // This is important for Chrome's NO_AUTH reuse or clients that always send user/pass.
     let proto = if opt.auth_once && auth_state.is_whitelisted(client_ip).await {
-        debug!("IP {} is whitelisted, skipping auth negotiation", client_ip);
         Socks5ServerProtocol::skip_auth_this_is_not_rfc_compliant(socket)
     } else {
-        // Not whitelisted, do normal auth
         match &opt.auth {
             AuthMode::NoAuth if opt.skip_auth => {
                 Socks5ServerProtocol::skip_auth_this_is_not_rfc_compliant(socket)
             }
             AuthMode::NoAuth => Socks5ServerProtocol::accept_no_auth(socket).await?,
             AuthMode::Password { username, password } => {
-                let (proto, auth_success) = Socks5ServerProtocol::accept_password_auth(socket, |user, pass| {
-                    user == *username && pass == *password
-                }).await?;
+                let (proto, auth_success) =
+                    Socks5ServerProtocol::accept_password_auth(socket, |user, pass| {
+                        user == *username && pass == *password
+                    })
+                    .await?;
 
-                // If auth_once enabled and auth success, whitelist IP
                 if opt.auth_once && auth_success {
                     auth_state.add_ip(client_ip).await;
                 }
@@ -168,13 +154,13 @@ async fn serve_socks5(
         }
     };
 
-    let (cmd, target_addr) = proto.read_command().await?.resolve_dns().await?;
+    let (proto, cmd, target_addr) = proto.read_command().await?.resolve_dns().await?;
 
     match cmd {
-        Socks5Command::TCPConnect => {
+        Socks5Command::TcpConnect => {
             run_tcp_proxy(proto, &target_addr, opt.request_timeout, false).await?;
         }
-        Socks5Command::UDPAssociate if opt.allow_udp => {
+        Socks5Command::UdpAssociate if opt.allow_udp => {
             let reply_ip = opt.public_addr.context("invalid reply ip")?;
             run_udp_proxy(proto, &target_addr, None, reply_ip, None).await?;
         }
@@ -192,8 +178,6 @@ where
     F: Future<Output = Result<()>> + Send + 'static,
 {
     task::spawn(async move {
-        if let Err(err) = fut.await {
-            error!("{:#}", &err);
-        }
+        let _ = fut.await;
     })
 }
